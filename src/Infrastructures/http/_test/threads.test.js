7552f1f81cf6fd868c09f3eaa580f28bd18c5bcf
yazid138 <ma39isy@gmail.com>
2023-02-26 12:30:56 +0700

    commit

--- src/Infrastructures/http/_test/threads.test.js
+++ src/Infrastructures/http/_test/threads.test.js
@@ -1,6 +1,8 @@
 const pool = require('../../database/postgres/pool');
 const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
 const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const RepliesTableTestHelper = require('../../../../tests/RepliesTableTestHelper');
 const container = require('../../container');
 const createServer = require('../createServer');
 
@@ -8,7 +10,39 @@ describe('/threads endpoint', () => {
 
     let server = null;
     let accessToken = '';
+    let user = {};
+    let thread = {};
+    let comment = {};
+    let reply = {};
     beforeAll(async () => {
+        user = await UsersTableTestHelper.addUser({
+            id: 'user-123',
+            fullname: 'Dicoding Indonesia',
+            username: 'dicoding',
+            password: 'rahasia',
+        })
+
+        thread = await ThreadsTableTestHelper.addThread({
+            id: 'thread-123',
+            title: 'ini adalah title',
+            body: 'ini adalah body',
+            userId: user.id,
+        })
+
+        comment = await CommentsTableTestHelper.addComment({
+            id: 'comment-123',
+            content: 'ini adalah komentar',
+            userId: user.id,
+            threadId: thread.id,
+        })
+
+        reply = await RepliesTableTestHelper.addReply({
+            id: 'reply-123',
+            commentId: comment.id,
+            userId: user.id,
+            content: 'ini adalah balasan komentar',
+        })
+
         server = await createServer(container);
 
         // add user
@@ -36,6 +70,8 @@ describe('/threads endpoint', () => {
     })
 
     afterAll(async () => {
+        await RepliesTableTestHelper.cleanTable();
+        await CommentsTableTestHelper.cleanTable();
         await ThreadsTableTestHelper.cleanTable();
         await UsersTableTestHelper.cleanTable();
         await pool.end();
@@ -48,8 +84,8 @@ describe('/threads endpoint', () => {
                 method: 'POST',
                 url: '/threads',
                 payload: {
-                    title: 'dicoding',
-                    body: 'secret',
+                    title: 'ini adalah judul',
+                    body: 'ini adalah body',
                 },
                 headers: {
                     Authorization: `Bearer ${accessToken}`
@@ -178,4 +214,251 @@ describe('/threads endpoint', () => {
         //     expect(responseJson.message).toEqual('username tidak tersedia');
         // });
     });
+
+    describe('when GET /threads/{threadId}', () => {
+
+        it('404', async () => {
+            const response = await server.inject({
+                method: 'GET',
+                url: `/threads/xxx`,
+            });
+
+            // Assert
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(404);
+            expect(responseJson.status).toEqual('fail');
+            expect(responseJson.message).toEqual('thread_id tidak ada');
+        })
+
+        it('200', async () => {
+            const response = await server.inject({
+                method: 'GET',
+                url: `/threads/${thread.id}`,
+            });
+
+            // Assert
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(200);
+            expect(responseJson.status).toEqual('success');
+            expect(responseJson.data.thread).toBeDefined();
+        })
+    })
+
+    describe('when POST /threads/{threadId}/comments', () => {
+        it('401', async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/${thread.id}/comments`,
+                payload: {
+                    content: 'bagus sekali!',
+                },
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(401);
+            expect(responseJson.error).toEqual('Unauthorized');
+            expect(responseJson.message).toEqual('Missing authentication');
+        })
+
+        it('404', async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/xxx/comments`,
+                payload: {
+                    content: 'bagus sekali!',
+                },
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(404);
+            expect(responseJson.status).toEqual('fail');
+            expect(responseJson.message).toEqual('thread_id tidak ada');
+        })
+
+        it('400', async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/${thread.id}/comments`,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(400);
+            expect(responseJson.status).toEqual('fail');
+            expect(responseJson.message).toEqual('tidak dapat membuat comment baru karena properti yang dibutuhkan tidak ada');
+        })
+
+        it('400', async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/${thread.id}/comments`,
+                payload: {
+                    content: 123,
+                },
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(400);
+            expect(responseJson.status).toEqual('fail');
+            expect(responseJson.message).toEqual('tidak dapat membuat comment baru karena tipe data tidak sesuai');
+        })
+
+        it('200', async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/${thread.id}/comments`,
+                payload: {
+                    content: 'bagus sekali!',
+                },
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(201);
+            expect(responseJson.status).toEqual('success');
+            expect(responseJson.data.addedComment).toBeDefined();
+        })
+    })
+
+    describe('when POST /threads/{threadId}/comments/{commentId}/replies', () => {
+        it('200', async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/${thread.id}/comments/${comment.id}/replies`,
+                payload: {
+                    content: 'bagus sekali!',
+                },
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(201);
+            expect(responseJson.status).toEqual('success');
+            expect(responseJson.data.addedReply).toBeDefined();
+        })
+    })
+
+    describe('when DELETE /threads/{threadId}/comments/{commentId}', () => {
+
+        let newComment = {}
+        beforeAll(async() => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/${thread.id}/comments`,
+                payload: {
+                    content: 'bagus sekali!',
+                },
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            newComment = JSON.parse(response.payload).data.addedComment;
+        })
+
+        it('403', async () => {
+            const response = await server.inject({
+                method: 'DELETE',
+                url: `/threads/${thread.id}/comments/${comment.id}`,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(403);
+            expect(responseJson.status).toEqual('fail');
+            expect(responseJson.message).toEqual('user dilarang');
+        })
+
+        it('403', async () => {
+            const response = await server.inject({
+                method: 'DELETE',
+                url: `/threads/${thread.id}/comments/${comment.id}`,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(403);
+            expect(responseJson.status).toEqual('fail');
+            expect(responseJson.message).toEqual('user dilarang');
+        })
+
+        it('200', async () => {
+            const response = await server.inject({
+                method: 'DELETE',
+                url: `/threads/${thread.id}/comments/${newComment.id}`,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(200);
+            expect(responseJson.status).toEqual('success');
+        })
+    })
+
+    describe('when DELETE /threads/{threadId}/comments/{commentId}/replies/{replyId}', () => {
+        
+        let newReply = {}
+        beforeAll(async () => {
+            const response = await server.inject({
+                method: 'POST',
+                url: `/threads/${thread.id}/comments/${comment.id}/replies`,
+                payload: {
+                    content: 'bagus sekali!',
+                },
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            newReply = JSON.parse(response.payload).data.addedReply;
+        })
+
+        it('403', async () => {
+            const response = await server.inject({
+                method: 'DELETE',
+                url: `/threads/${thread.id}/comments/${comment.id}/replies/${reply.id}`,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(403);
+            expect(responseJson.status).toEqual('fail');
+            expect(responseJson.message).toEqual('user dilarang');
+        })
+
+        it('200', async () => {
+            const response = await server.inject({
+                method: 'DELETE',
+                url: `/threads/${thread.id}/comments/${comment.id}/replies/${newReply.id}`,
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(200);
+            expect(responseJson.status).toEqual('success');
+        })
+
+    })
 });
